5ce421f9be3456c873d1ad81babe1fd88f272fe1
Tom Aldcroft <taldcroft@gmail.com>
2008-12-30 15:05:12 +0000

    Added smooth() and interpolate() functions

--- Ska/Numpy.py
+++ Ska/Numpy.py
@@ -115,3 +115,84 @@ def filter(recarray, filters):
     else:
         return recarray
 
+def interpolate(yin, xin, xout, method='linear'):
+    """Interpolate the curve defined by (xin, yin) at points xout.  The array
+    xin must be monotonically increasing.  The output has the same data type as
+    the input yin.
+
+    @param yin: y values of input curve
+    @param xin: x values of input curve
+    @param xout: x values of output interpolated curve
+    @param method: interpolation method ('linear' | 'nearest')
+
+    @return: numpy array with interpolated curve
+    """
+    yout = numpy.empty(len(xout), dtype=yin.dtype)
+    lenxin = len(xin)
+
+    i1 = numpy.searchsorted(xin, xout)
+    i1[ i1==0 ] = 1
+    i1[ i1==lenxin ] = lenxin-1
+
+    x0 = xin[i1-1]
+    x1 = xin[i1]
+    y0 = yin[i1-1]
+    y1 = yin[i1]
+
+    if method == 'linear':
+        return (xout - x0) / (x1 - x0) * (y1 - y0) + y0
+    elif method == 'nearest':
+        return numpy.where(numpy.abs(xout - x0) < numpy.abs(xout - x1), y0, y1)
+    else:
+        raise ValueError('Invalid interpolation method: %s' % method)
+
+def smooth(x,window_len=10,window='hanning'):
+    """Smooth the data using a window with requested size.
+    
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+    
+    Example::
+      t = linspace(-2, 2, 50)
+      y = sin(t) + randn(len(t)) * 0.1
+      ys = Ska.Numpy.smooth(y)
+      plot(t, y, t, ys)
+    
+    See also::
+      numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+      scipy.signal.lfilter
+
+    @param x: input signal 
+    @param window_len: dimension of the smoothing window
+    @param window: type of window ('flat', 'hanning', 'hamming', 'bartlett', 'blackman')
+
+    @return: smoothed signal
+    """
+
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        raise ValueError, "Input vector needs to be bigger than window size."
+
+
+    if window_len<3:
+        return x
+
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+
+    s=numpy.r_[2*x[0]-x[window_len:1:-1],x,2*x[-1]-x[-1:-window_len:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=numpy.ones(window_len,'d')
+    else:
+        w=eval('numpy.'+window+'(window_len)')
+
+    y=numpy.convolve(w/w.sum(),s,mode='same')
+    return y[window_len-1:-window_len+1]
+
