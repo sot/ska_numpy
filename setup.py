157a153ca6ae895bafdc30ccd242b68b3b32b374
Tom Aldcroft <taldcroft@gmail.com>
2012-06-02 18:23:15 -0400

    Add fastss.pyx - fast search sorted when both arrays are sorted

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup
+
+import numpy as np
+from setuptools.extension import Extension
+from Cython.Distutils import build_ext
 from Ska.Numpy.version import version
 
+cmdclass = {'build_ext': build_ext}
+fastss_ext = Extension("Ska.Numpy.fastss",
+                       ['Ska/Numpy/fastss.pyx'],
+                       include_dirs=[np.get_include()])
 
 setup(name='Ska.Numpy',
-      author = 'Tom Aldcroft',
+      author='Tom Aldcroft',
       description='Numpy utilities',
-      author_email = 'taldcroft@cfa.harvard.edu',
+      author_email = 'aldcroft@head.cfa.harvard.edu',
       py_modules = ['Ska.Numpy'],
       version=version,
+      cmdclass=cmdclass,
+      ext_modules=[fastss_ext],
       zip_safe=False,
       namespace_packages=['Ska'],
       packages=['Ska', 'Ska/Numpy'],
